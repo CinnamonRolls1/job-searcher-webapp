f956d306a367e97607fee9807e1804c13ed41aa7
Safeer <safeerahmed.v@gmail.com>
2019-10-12 13:08:24 +0300

    send search query back to server and added templates for results

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const path= require('path');
-
+const bodyParser = require('body-parser');
 var app = express();
 
 //Load view engine
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine','pug');
 
+//Body-Parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 //GET for Home route
 app.get('/', function(req,res){
   res.render('home');
 });
+
+app.post('/', function(req, res){
+  //get form data
+  var role = req.body.role;
+  var location = req.body.location;
+  console.log(role,location);
+  res.redirect('/results');
+})
+
+var jobs =  [
+  { title : 'Software engineer', desc : 'random', salary : 10000},
+  { title : 'Back end', desc : 'random', salary : 10000},
+  { title : 'Cloud Reliabilty', desc : 'random', salary : 10000},
+  { title : 'Compiler Research', desc : 'random', salary : 10000},
+  { title : 'Full Stack', desc : 'random', salary : 10000},
+  { title : 'Content Creator', desc : 'random', salary : 10000},
+  { title : 'Server Technology', desc : 'random', salary : 10000},
+  { title : 'Manager', desc : 'random', salary : 10000},
+  { title : 'Project Head', desc : 'random', salary : 10000}
+]
+
 app.get('/results', function(req,res) {
-  res.render('results');
+  res.render('results',{job_desc : jobs});
 });
 
 //Start Server
