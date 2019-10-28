580832d5578985884ed7ea1e85d9a5e1f71a9d62
Chinmayan Pradeep <chinmayanks@gmail.com>
2019-10-28 19:05:40 +0530

    Salary now processed. All salaries are extracted from strings. Code automatically detects and converts salaries into annual if monthly. Double is returned. Additionally, ranges are detected and automatically converted into simple averages.

--- scraper/scraper/spiders/jobs.py
+++ scraper/scraper/spiders/jobs.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 # running: scrapy crawl jobs -o data.csv
 
@@ -22,16 +23,35 @@ class JobsSpider(scrapy.Spider):
 	def parse(self, response):
 		for job in response.xpath("//div[contains(@class,'jobsearch-SerpJobCard') and contains(@class,'unifiedRow')]"):
 			
+			##### COMPANY NAME
 			company=job.xpath("normalize-space(.//span[@class='company']/text())") 
 			# print("company:", company.extract_first())
 			if company.extract_first() == '':
 				company=job.xpath("normalize-space(.//a[@class='turnstileLink']/text())")
 
+			##### SALARY
+			salary_data=job.xpath("normalize-space(.//span[contains(@class, 'salary')])")
+			salary=salary_data.re(r'[₹][A-Za-z0-9,]+')
+			if salary:
+				if len(salary)==2:
+					low_sal=float(re.sub('[,₹]','',salary[0]))
+					high_sal=float(re.sub('[,₹]','',salary[1]))
+					salary=(low_sal+high_sal)/2
+					# print("Salary averaged!")
+				else:
+					salary=salary[0]
+					salary=int(re.sub('[,₹]','',salary))
+				# print(salary)
+				if salary_data.re(r'[mM][oO][nN][tT][hH]'):
+					# print("\nMonthly salary detected!")
+					salary=12*salary
+
+
 			yield {
 				'TITLE' : job.xpath("normalize-space(.//div[@class='title']/a/text())").extract_first(),
 				'COMPANY' : company.extract_first(),
 				'LOCATION' : job.xpath("normalize-space(.//div[contains(@class,'location')])").extract_first(),
-				'SALARY' : job.xpath("normalize-space(.//span[contains(@class, 'salary')])").extract_first(),
+				'SALARY' : salary,
 				# 'AGE OF POSTING' : job.xpath("normalize-space(.//span[@class='date '])").extract_first()
 			}
 
